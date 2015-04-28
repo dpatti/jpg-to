80eedc2ac583dc1891ca32bdd72de513d7a550e7
Doug Patti <douglas.patti@gmail.com>
2015-04-27 21:04:19 -0400

    initial commit

---
+++ Main.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE OverloadedStrings, NoImplicitPrelude #-}
+
+import           BasePrelude
+import           Control.Lens
+import           Data.Aeson.Lens
+import qualified Data.Configurator as Conf
+import           Data.Configurator.Types (Configured, Name)
+import           Data.Text (Text, pack, unpack)
+import qualified Network.Wreq as Wreq
+import           System.Random
+
+data Gapi = Gapi { apiKey :: Text, cx :: Text }
+
+type PartialQuery = Wreq.Options -> Wreq.Options
+
+imgApiRoot :: String
+imgApiRoot = "https://www.googleapis.com/customsearch/v1"
+
+-- https://developers.google.com/custom-search/json-api/v1/reference/cse/list
+imgApiQuery :: Gapi -> PartialQuery
+imgApiQuery s = (Wreq.param "key" .~ [apiKey s])
+              . (Wreq.param "cx" .~ [cx s])
+              . (Wreq.param "searchType" .~ ["image"])
+              . (Wreq.param "safe" .~ ["high"])
+              . (Wreq.param "imgSize" .~ ["large"])
+
+findBest :: Gapi -> Text -> IO Text
+findBest gapi query = do
+  let opts = Wreq.defaults
+           & imgApiQuery gapi
+           & (Wreq.param "q" .~ [query])
+  r <- Wreq.getWith opts imgApiRoot
+  let items = toList . fromJust . (r ^?) $ Wreq.responseBody . key "items" . _Array
+  rand <- randomIO :: IO Int
+  let pick = items !! (rand `mod` length items)
+
+  return . fromJust . (pick ^?) $ key "link" . _String
+
+conf :: Configured a => Name -> IO a
+conf name = do
+  config <- Conf.load [Conf.Required "conf"]
+  Conf.require config name
+
+main :: IO ()
+main = do
+  gapi <- Gapi <$> conf "key" <*> conf "cx"
+
+  args <- getArgs
+  case args of
+    [] -> do
+      putStrLn "Usage: jpg-cli <query>"
+      exitWith $ ExitFailure 1
+    query:_ -> do
+      url <- findBest gapi (pack query)
+      putStrLn . unpack $ url
---
+++ README.md
@@ -0,0 +1,38 @@
+# jpg-cli
+
+```
+$ jpg-cli "haskell logo"
+https://wiki.haskell.org/wikiupload/6/6e/Haskell_logo_falconnl_8_fancy.png
+```
+
+It doesn't actually only limit to jpg files. Everything else is hard-coded.
+Errors are not handled. I don't know how to write Haskell.
+
+## Setup
+
+```
+$ cp conf.sample conf
+$ cabal sandbox init
+$ cabal install -j
+$ cabal build
+```
+
+Put your public API access key and custom search engine id in `conf`.
+
+### Getting an API key
+
+1. https://console.developers.google.com/project
+2. Create project
+3. APIs & auth > APIs > Custom Search API > Enable
+4. APIs & auth > Credentials > Create new Key > Server key
+
+### Getting the search engine id
+
+1. https://cse.google.com/cse/all
+2. Add
+3. Pick a random site to search; name the search engine; create
+4. Edit your search engine
+5. Remove the site if you want
+6. Change "Search only included sites" to "Search the entire web but emphasize
+   included sites"
+7. Click "Search engine ID"
